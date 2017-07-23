eabe3664a4f1f1e18e461077d64bc7f7b2eedc59
James Lovallo <patrickonparker@gmail.com>
2017-07-23 18:32:44 +0000

    Crazy Carousel Test

--- proposals/degc/js/init.js
+++ proposals/degc/js/init.js
@@ -1,16 +1,16 @@
 (function ($) {
   $(function () {
-
-    $('.button-collapse').sideNav();
     $('.modal').modal();
+    var carousel_interval = 10000;
     $('.carousel').carousel({
       dist: -400,
       shift: 300,
       indicators: true,
-      interval: 8000,
     });
+    setInterval(function () {
+      $('.carousel').carousel('next');
+    }, carousel_interval);
     $('.parallax').parallax();
-    $('select').material_select();
-
+    $('.carousel').carousel('next');
   }); // end of document ready
 })(jQuery); // end of jQuery name space
