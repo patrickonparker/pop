7e149affd84caaf66335f2269a69ddbaf016b256
James Lovallo <patrickonparker@gmail.com>
2017-07-23 17:27:10 +0000

    Tweaked javascript handling, let's hope this works!

--- proposals/degc/js/init.js
+++ proposals/degc/js/init.js
@@ -1,7 +1,16 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
+    $('.modal').modal();
+    $('.carousel').carousel({
+      dist: -400,
+      shift: 300,
+      indicators: true,
+      interval: 8000,
+    });
+    $('.parallax').parallax();
+    $('select').material_select();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
