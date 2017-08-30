754ef1c7a1db37381e989be1ec7bfb069e613db9
James Lovallo <patrickonparker@gmail.com>
2017-08-30 13:27:29 +0000

    Cleaned up javascript in init.js, shortened autoplay time.

--- js/init.js
+++ js/init.js
@@ -1,16 +1,16 @@
-(function ($) {
-  $(function () {
-    $('.modal').modal();
-    var carousel_interval = 10000;
-    $('.carousel').carousel({
-      dist: -400,
-      shift: 300,
-      indicators: true,
-    });
-    setInterval(function () {
-      $('.carousel').carousel('next');
-    }, carousel_interval);
-    $('.parallax').parallax();
+$(document).ready(function () {
+  var carousel_interval = 10000;
+  $('.modal').modal();
+  $('.parallax').parallax();
+  $('.carousel').carousel({
+    dist: -400,
+    shift: 300,
+    indicators: true,
+  });
+
+  function autoplay() {
     $('.carousel').carousel('next');
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    setTimeout(autoplay, 4500);
+  }
+  autoplay()
+}); // end of document ready
