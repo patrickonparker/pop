6a448749de1f346aede2054abe8ee2bf3354336c
James Lovallo <patrickonparker@gmail.com>
2017-10-12 16:40:38 -0400

    Removed document ready from init.js

--- js/init.js
+++ js/init.js
@@ -1,17 +1,15 @@
-$(document).ready(function () {
-    var carousel_interval = 10000;
-    $('.modal').modal();
-    $('ul.tabs').tabs();
-    $('.parallax').parallax();
-    $('.carousel').carousel({
-        dist: -400
-        , shift: 300
-        , indicators: true
-    , });
+var carousel_interval = 10000;
+$('.modal').modal();
+$('ul.tabs').tabs();
+$('.parallax').parallax();
+$('.carousel').carousel({
+    dist: -400
+    , shift: 300
+    , indicators: true
+, });
 
-    function autoplay() {
-        $('.carousel').carousel('next');
-        setTimeout(autoplay, 4500);
-    }
-    autoplay()
-}); // end of document ready
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+autoplay()
